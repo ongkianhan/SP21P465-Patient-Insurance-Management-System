18efbf330898787f9d01b9bacc78e1e31f20612a
pfyffe <pfyffe@iu.edu>
2021-03-06 15:33:56 -0500

    Customized DoctorCard

--- patient-manager-react/src/components/UserSearch/DoctorCard.js
+++ patient-manager-react/src/components/UserSearch/DoctorCard.js
@@ -11,23 +11,23 @@ class DoctorCard extends Component
         return (
             <div className="container">
             <div className="card card-body bg-light mb-3">
-                <div className="row">
+                <div className="row align-items-center">
                     <div className="col-3">
                         <img className="col mh-25" src={defaultProfileIcon} />
                     </div>
                     <div className="col-6 text-left">
                         <h3>{doctor.firstName}{" "}{doctor.lastName}</h3>
-                        <h4>{doctor.specialty}</h4>
+                        <h4>{doctor.specialization}</h4>
                         <h4>{doctor.hospitalName}</h4>
                     </div>
                     <div className="col-3 justify-content-end">
                         <Link to={`/appointments/${doctor.userId}`}>
-                            <button className="col my-1 rounded bg-primary-button">
+                            <button className="col my-1 primary-button card-button">
                                 Make Appointment
                             </button>
                         </Link>
                         <Link to={`/account/${doctor.userId}`}>
-                            <button className="col my-1 rounded bg-secondary-button">
+                            <button className="col my-1 secondary-button card-button">
                                 View Profile
                             </button>
                         </Link>
--- patient-manager-react/src/components/UserSearch/DoctorSearch.js
+++ patient-manager-react/src/components/UserSearch/DoctorSearch.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import { getAllDoctors } from "../../actions/userActions";
 import { PropTypes } from "prop-types"
 import DoctorCard from "./DoctorCard";
+import classnames from "classnames";
 
 class DoctorSearch extends Component 
 {
@@ -19,11 +20,30 @@ class DoctorSearch extends Component
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
-                            <h1 className="display-4 text-center">Doctors</h1>
+                            <h1 className="display-4 text-left page-header">Find Doctors</h1>
+                            
+                            <div className="row align-items-center">
+                                <div className="col-10">
+                                    <textarea 
+                                    className={classnames("form-control")}
+                                    placeholder="Search by keywords..."
+                                    rows="1"
+                                    onKeyPress={e => {
+                                        if(e.key === 'Enter')
+                                            e.preventDefault()
+                                        }}
+                                    ></textarea>
+                                </div>
+                                <div className="col-2 justify-content-end">
+                                    <button type="submit" className="card-button primary-button">
+                                        Search
+                                    </button>
+                                </div>
+                            </div>
+
                             <br />
                             
                             <br />
-                            <hr />
                             {/* Get the entire list of doctors. For each DoctorCard, set its prop as the doctor data */}
                             {allDoctors.map(doctor => (
                                 <DoctorCard key={doctor.id} doctor={doctor} />
